f0c84bb2c502b93f909ced6443eb9de0614389b7
ychinlee.2@gmail.com <ychinlee.2@gmail.com>
2016-01-07 20:04:45 -0500

    no compiling errors yet

--- 8/ying-ke_chin-lee/display.h
+++ 8/ying-ke_chin-lee/display.h
@@ -6,28 +6,78 @@
 #define browser "chromium-browser"
 
 void display(Lot l) {
-	char *cmds[2] = {browser, l.url};
+	char *cmds[3] = {browser, l.url, NULL};
 	
 	printf("cmds[0] = %s\n", cmds[0]);
 	printf("cmds[1] = %s\n", cmds[1]);
 	int f = fork();
 	if (f == 0) {	
-		//int fd1 = creat(cmds[0], 0644);
-	        //dup2(fd1, 1);
-        	//close(fd1);
 		print(l);
 
-		printf("View the work? Y/N\n");
-		// get input, if Y, then load browser. else, don't.
+		char ans[1];
+
+		printf("View the work? y/n: ");
+		scanf("%s", ans);
+		if (strcmp(ans, "y") != 0) {
+			printf("neq\n");
+			exit(0);
+		}
+
+		// get input, if "y", then load browser. else, don't.
 		int num = execvp(browser, cmds);
 		if (num < 0) {
 			printf("error %d happened: %s\n", errno, strerror(errno));
 		}
-		// print stats
-//		exit(0);
+		exit(0);
 	} else {
 		int status;
 		wait(&status);
+	}
+}
+
+void bid() {
+	int status;
+	char ans[1];
+	printf("Bid on this work? (y/n)\n");
+	scanf("%s", ans);
+	if (strcmp(ans, "y") != 0) {
+		// do nothing.
+		printf("Chose to do nothing.\n");
+		exit(0);
+	} else {
+		//open bid
+		int f = fork();
+		if (f == 0) {
+			char *creat[3] = {"./control", "-c", NULL};
+			int f0 = fork();
+			if (f0 == 0) {
+				execvp(creat[0], creat);
+				exit(0);
+			}
+			else wait(&status);
+			
+			// start the writing program, which will write bids
+			char *cmds[2] = {"./writing", NULL};
+			int f1 = fork();
+			if (f1 == 0) {
+				execvp(cmds[0], cmds);
+				exit(0);
+			} else {
+				wait(&status);
+			}
+
+			// close bid info
+			char *remv[3] = {"./control", "-r", NULL};
+			int f2 = fork();
+			if (f2 == 0) {
+				execvp(remv[0], remv);
+				exit(0);
+			} else {
+				wait(&status);
+			}
+		} else {
+			wait(&status);
+		}
 	}
 }
 

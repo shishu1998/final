a193376a1046d877e2139f44bf5ff1d0f436eba1
Aidan Fitzgerald <duckjr159@gmail.com>
2016-01-14 13:56:36 -0500

    Started client and server

---
+++ 5/margaretburkart_aidanfitzgerald/lib.h
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include <stdio.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+
+void client_talk(int);
+
+void server_talk(int);

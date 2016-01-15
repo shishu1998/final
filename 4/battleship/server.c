96b3380bf7075813b325ec0a7356f7974e89814e
Max Kong <maxkong99@gmail.com>
2016-01-15 14:02:44 -0500

    added makefile for servers

--- 4/battleship/server.c
+++ 4/battleship/server.c
@@ -21,14 +21,18 @@ int main() {
   listener.sin_port = htons(24601); //port #
   listener.sin_addr.s_addr = INADDR_ANY; //bind to any incoming address
   bind(socket_id, (struct sockaddr *)&listener, sizeof(listener));
-  
-  listen( socket_id, 1 );
-  printf("<server> listening\n");
+  while(1){  
+    listen( socket_id, 1 );
+    printf("<server> listening\n");
 
-  socket_client = accept( socket_id, NULL, NULL );
-  printf("<server> connected: %d\n", socket_client );
-
-  write( socket_client, "hello", 6 );
-  
+    socket_client = accept( socket_id, NULL, NULL );
+    printf("<server> connected: %d\n", socket_client );
+    if( socket_client % 2 ){
+      write( socket_client, "P2:", 6 );
+    }
+    else{
+      write( socket_client, "P1:", 6 );
+    }
+  }
   return 0;
 }

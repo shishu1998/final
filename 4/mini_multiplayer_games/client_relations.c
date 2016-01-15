fa905c0ecac303cd18c632af99ee66112e452d2a
Sammi <sammiwuleung@gmail.com>
2016-01-15 10:38:47 -0500

    update

---
+++ 4/mini_multiplayer_games/client_relations.c
@@ -0,0 +1,12 @@
+void initiate_text() {
+  printf("Hello, welcome!\n");
+  printf("Type in help if you're not sure what to do");
+}
+
+void print_help() {
+
+  printf("To initiate a new game:\n");
+  printf("Type 1 for Connect 4\n");
+  printf("Type 2 for Tic-Tac-Toe\n");
+  printf("Type 3 for Connect the Dots\n");
+}

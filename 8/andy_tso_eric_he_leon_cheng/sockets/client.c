a5954f26ad4d8d1d20d61e58f7262931b883eae1
TESLA <57leonardo@gmail.com>
2016-01-15 13:47:02 -0500

    tried multiple connections with sockets

--- 8/andy_tso_eric_he_leon_cheng/sockets/client.c
+++ 8/andy_tso_eric_he_leon_cheng/sockets/client.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <errno.h>
 
 #include <arpa/inet.h>
 #include <sys/types.h>
@@ -16,22 +17,26 @@ int main(int argc, char **argv) {
 
   //create the socket
   socket_id = socket( AF_INET, SOCK_STREAM, 0 );
-  
+
   //bind to port/address
   struct sockaddr_in sock;
-  sock.sin_family = AF_INET;   
+  sock.sin_family = AF_INET;
   sock.sin_port = htons(24601);
   //Set the IP address to connect to
   //127.0.0.1 is the "loopback" address of any machine
   inet_aton( "127.0.0.1", &(sock.sin_addr) );
   bind( socket_id, (struct sockaddr *)&sock, sizeof(sock));
-  
+
   //attempt a connection
   i = connect(socket_id, (struct sockaddr *)&sock, sizeof(sock));
   printf("<client> connect returned: %d\n", i);
+  printf("Error #%d: %s\n", errno, strerror(errno));
 
   read( socket_id, buffer, sizeof(buffer));
   printf("<client> received: [%s]\n", buffer );
-  
+
+  close(socket_id);
+
+
   return 0;
 }
--- 8/andy_tso_eric_he_leon_cheng/sockets/server.c
+++ 8/andy_tso_eric_he_leon_cheng/sockets/server.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <error.h>
 
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -11,6 +12,7 @@
 int main() {
 
   int socket_id, socket_client;
+
 
   //create the socket
   socket_id = socket( AF_INET, SOCK_STREAM, 0 );
@@ -22,18 +24,35 @@ int main() {
   listener.sin_addr.s_addr = INADDR_ANY; //bind to any incoming address
   bind(socket_id, (struct sockaddr *)&listener, sizeof(listener));
 
+  listen( socket_id, 1 );
+  printf("<server> listening\n");
 
+  int pid = 0;
+  
   while ( 1 ) {
-
-    listen( socket_id, 1 );
-    printf("<server> listening\n");
-
     socket_client = accept( socket_id, NULL, NULL );
+    if (socket_client < 0)
+      printf("ERROR on accept\n");
+    pid = fork();
+    if (pid < 0)
+      printf("ERROR on fork\n");
+    if (pid == 0)  {
+      close(socket_id);
+      printf("[child] eric sucks\n");
+      exit(0);
+    }
+    else close(socket_client);
+  }
+
     printf("<server> connected: %d\n", socket_client );
 
     write( socket_client, "hello", 6 );
 
-  }
+    close(socket_client);
+    close(socket_id);
+
+
+
 
   return 0;
 }

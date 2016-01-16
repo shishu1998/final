82dcc8159c04685090e3709916cb5053e022333f
Jennifer Lee <jennifer.lee.ny@gmail.com>
2016-01-16 10:16:58 -0500

    bug in mary.c

--- 8/JEM/mary.c
+++ 8/JEM/mary.c
@@ -11,14 +11,10 @@
 int hand_in_maryiage(int *from_suitor) {//file descriptor of client (read)
   int mary_server;//file descriptor of server end (write)
   char buffer[100];
-
   mkfifo("CHAT", 0644);//1
-  *from_suitor = open("CHAT", O_RDONLY);//2
-  
+  *from_suitor = open("CHAT", O_RDONLY);
   remove("CHAT");
-  
-  read(*from_suitor, buffer, sizeof(buffer));
-  
+  read(*from_suitor, buffer, sizeof(buffer));  
   mary_server = open(buffer, O_WRONLY);
   
   strcpy(buffer, "Let me take you to PROM!");
@@ -27,21 +23,21 @@ int hand_in_maryiage(int *from_suitor) {//file descriptor of client (read)
   return mary_server;
 }
 
-int main() {
-  
+int main() {  
   int mary_server, from_suitor;
   char buffer[100];
   //mary_server = hand_in_maryiage(&from_suitor);//returns file descriptor of pipe?
   while(1) {
     printf("<mary_server> waiting for connection...\n");
     mary_server = hand_in_maryiage(&from_suitor);//returns file descriptor of pipe?
-    while (read(from_suitor, buffer, sizeof(buffer))) {
-      printf("<mary_server> received [%s]\n", buffer);
+    printf("hand_in_marriage is complete\m");
+    While(from_suitor, buffer, sizeof(buffer))) {
+      printf("<mary_server> received: [%s]\n", buffer);
       write (mary_server, buffer, sizeof(buffer));
       strncpy(buffer, "", sizeof(buffer));
+      close (mary_server);
     }
   }
-  close(mary_server);
   close(from_suitor);
   return 0;
 }
--- 8/JEM/suitor.c
+++ 8/JEM/suitor.c
@@ -8,21 +8,21 @@
 #include <string.h>
 
 int suit_able(int *from_suitor) {//file descriptor of suitor(read)
-  int to_client ;
+  int to_mary;//file descriptor to open connection/pipe
   char buffer[100];
 
   sprintf(buffer, "%d\n", getpid()); //takes getpid into buffer
   mkfifo(buffer, 0644);
 
-  to_server = open("mario", O_WRONLY);
-  write(to_server, buffer, sizeof(buffer));
+  to_mary = open("CHAT", O_WRONLY);
+  write(to_mary, buffer, sizeof(buffer));
 
-  *from_server = open(buffer, O_RDONLY); // when this returns a value, we know the server has connected to the pipe
+  *from_suitor = open(buffer, O_RDONLY); // when this returns a value, we know the server has connected to the pipe
   remove(buffer); //removes named pipe so connection can't get high-jacked
-  read(*from_server, buffer, sizeof(buffer));
-  printf("<client> connection established: [%s]\n", buffer);
+  read(*from_suitor, buffer, sizeof(buffer));
+  printf("<suitor> connection established: [%s]\n", buffer);
 
-  return to_server;
+  return to_mary;
 }
 
 
@@ -30,13 +30,13 @@ int main() {
   int to_server, from_server;
   char buffer[100];
   
-  to_server = client_handshake(&from_server);
+  to_server = suit_able(&from_server);
   while (1) {
-    printf("<client> enterstuff:");
+    printf("<suitor> enter message:");
     fgets(buffer, sizeof(buffer), stdin);
     write(to_server, buffer, sizeof(buffer));
     read(from_server, buffer,sizeof(buffer) );
-    printf("<client> received: [%s]\n", buffer);
+    printf("<suitor> Mary sent a message: [%s]\n", buffer);
 
 }
 close (to_server);

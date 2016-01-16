e74aa8e6d37b7351be1a2361ac7a6a799cbb5575
Jennifer Lee <jennifer.lee.ny@gmail.com>
2016-01-16 10:57:32 -0500

    debugging mary.c

--- 8/JEM/mary.c
+++ 8/JEM/mary.c
@@ -11,13 +11,16 @@
 int hand_in_maryiage(int *from_suitor) {//file descriptor of client (read)
   int mary_server;//file descriptor of server end (write)
   char buffer[100];
-  mkfifo("CHAT", 0644);//1
+  
+  mkfifo("CHAT", 0644);
   *from_suitor = open("CHAT", O_RDONLY);
   remove("CHAT");
   read(*from_suitor, buffer, sizeof(buffer));  
   //mary_server = open(buffer, O_WRONLY);
   printf("<mary> connection is established: [%s]\n", buffer);
-  strcpy(buffer, "Let me take you to PROM!");
+  mary_server = open(buffer, O_WRONLY);
+
+  strncpy(buffer, "Let me take you to PROM!", sizeof(buffer));
   write(mary_server, buffer, sizeof(buffer));
 
   return mary_server;
@@ -30,13 +33,13 @@ int main() {
   while(1) {
     printf("<mary_server> waiting for connection...\n");
     mary_server = hand_in_maryiage(&from_suitor);//returns file descriptor of pipe?
-    printf("hand_in_marriage is complete\n");
+    
     while(read(from_suitor, buffer, sizeof(buffer))) {
       printf("<mary_server> received: [%s]\n", buffer);
       write (mary_server, buffer, sizeof(buffer));
       strncpy(buffer, "", sizeof(buffer));
-      close (mary_server);
     }
+    close(mary_server);
   }
   close(from_suitor);
   return 0;
--- 8/JEM/suitor.c
+++ 8/JEM/suitor.c
@@ -10,12 +10,12 @@
 int suit_able(int *from_suitor) {//file descriptor of suitor(read)
   int to_mary;//file descriptor to open connection/pipe
   char buffer[100];
-
   sprintf(buffer, "%d\n", getpid()); //takes getpid into buffer
   mkfifo(buffer, 0644);
-
   to_mary = open("CHAT", O_WRONLY);
   write(to_mary, buffer, sizeof(buffer));
+
+  printf("waiting for connection...\n");
 
   *from_suitor = open(buffer, O_RDONLY); // when this returns a value, we know the server has connected to the pipe
   remove(buffer); //removes named pipe so connection can't get high-jacked
@@ -31,15 +31,15 @@ int main() {
   char buffer[100];
   
   to_server = suit_able(&from_server);
+
   while (1) {
     printf("<suitor> enter message:");
     fgets(buffer, sizeof(buffer), stdin);
     write(to_server, buffer, sizeof(buffer));
     read(from_server, buffer,sizeof(buffer) );
     printf("<suitor> Mary sent a message: [%s]\n", buffer);
+  }
 
-}
-close (to_server);
-close(from_server);
-
+  close (to_server);
+  close(from_server); 
 }

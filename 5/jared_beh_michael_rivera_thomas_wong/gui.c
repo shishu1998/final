8815b6fbada6c7bbe09374642ee0fff01735d2b3
MGRiv <mgrivera501@gmail.com>
2016-01-19 06:51:05 -0500

    claswork

--- 5/jared_beh_michael_rivera_thomas_wong/gui.c
+++ 5/jared_beh_michael_rivera_thomas_wong/gui.c
@@ -4,8 +4,20 @@
 #include <fcntl.h>
 #include <string.h>
 
-char *clientformat(char *position,char player,char* board){
-  char *ret = "";
+char *clientformat(char* input){
+  if(input[0] == 'S'){
+    //Text output starting with Server: ...
+    printf("%s",input);
+  }else if(input[0] == 'm'){
+    //Move should update board
+    updateboard(input[1],input[2],input[3]);
+  }else if(input[0] == 'w'){
+    //Win condition update
+    winplayer = 
+  }
+  return boardstate;
+}
 
-  return 0;
+
+char *serverformat(char *input){
 }

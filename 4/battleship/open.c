32616896452e3a898e52f9a13c9639d757a0ffd2
lian wong <ltaylor.wong@gmail.com>
2016-01-19 01:10:27 -0500

    open screen

---
+++ 4/battleship/open.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void welcomeScreen(void){
+  printf("OOOOOO\n");
+  printf("OO   OO\n");
+  printf("OO  OO\n");
+  printf("OOOOOO\n");
+  printf("OO   OO\n");
+  printf("OOOOOO\n\n");
+  printf(" OOOO\n\n");
+  printf("OO  OO\n");
+  printf("OO  OO\n");
+  printf("OOOOOO\n");
+  printf("OO  OO\n");
+  printf("OO  OO\n");
+  printf("OOOOOO\n");
+  printf("  OO  \n");
+  printf("  OO\n");
+  printf();
+  printf();
+  printf();
+  printf();
+  printf();
+  printf();
+  printf();
+  printf();
+  printf();
+  printf();
+  printf();
+  printf();
+  printf();
+  printf();
+  printf();
+  printf();

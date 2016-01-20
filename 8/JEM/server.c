24c9d91455581899a91c34ab34cdd66b8133e1bf
Jennifer Lee <jennifer.lee.ny@gmail.com>
2016-01-20 13:58:41 -0500

    makefile

--- 8/JEM/server.c
+++ 8/JEM/server.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 int main() {
 

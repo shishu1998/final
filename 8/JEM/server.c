f711b23f69b68e39b9b54b0eab05ce2220c291e6
Jennifer Lee <jennifer.lee.ny@gmail.com>
2016-01-20 13:42:39 -0500

    work

--- 8/JEM/server.c
+++ 8/JEM/server.c
@@ -8,4 +8,24 @@
 
 int main() {
 
+int socket_id, socket_client;
+
+socket_id = socket( AF_INET, SOCK_STREAM, 0 );
+
+struct sockaddr_in listener;
+
+listener.sin_family = AF_INET;  //socket type IPv4
+listener.sin_port = htons(24601); //port #
+listener.sin_addr.s_addr = INADDR_ANY; //bind to any incoming address
+bind(socket_id, (struct sockaddr *)&listener, sizeof(listener));
+  
+listen( socket_id, 1 );
+printf("<server> listening\n");
+
+socket_client = accept( socket_id, NULL, NULL );
+printf("<server> connected: %d\n", socket_client );
+
+write( socket_client, "hello", 6 );
+  
+return 0;
 }

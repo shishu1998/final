e25c42e26e7f8ba63b6e9652337c93fe1730ee5c
lorenrose1013 <lorenrose1013@aol.com>
2016-01-21 07:29:13 -0500

    jukebox server done ?

--- 5/jukebox/jukebox.c
+++ 5/jukebox/jukebox.c
@@ -17,25 +17,55 @@ int send_song(char * song_name, int socket_client);
 int handle_client(int socket_client){
   char buf[64];
   while (  read(socket_client, buf, sizeof(buf))){
-    if (strcmp(buf, "l") == 0){
+    char substr[1];
+    memcpy (substr, &buf[0], 1);
+    substr[1] = '\n';
+    if (strcmp(substr, "l") == 0){
       //send song listings
       list_songs(socket_client);
     }
-    if (strcmp(buf, "q") == 0){
+    if (strcmp(substr, "q") == 0){
       exit(0);
     }
-    if (strcmp((char*)buf[0], "p") == 0){
+    if (strcmp(substr, "p") == 0){
       send_song(buf, socket_client);
     }
-    
   }
 
 }
 
-int send_song(char * song_name, int socket_client){
+int send_song(char * user_input, int socket_client){
     char song[5242880]; //5 mb is enough right?
-    int song_file = open(song_name, O_RDONLY);
-    read(song_file, song, sizeof(song));
+    char substr[3];
+    memcpy(substr, &user_input[2], 3); //get 3 dig song num
+    substr[3] = '\0';
+    char song_title[64];
+    //loop through songs and find corresponding title
+    DIR * music_dir = opendir("music");
+    struct dirent *file;
+    int i = 0;
+    while((file = readdir(music_dir))){
+      if (i > 1){ //skip the . and ..
+	char * test_title = file->d_name;
+	char sub_title[3]
+	memcpy(sub_title, test_title[0], 3);
+	sub_title[3] = '\0';
+	if (strcmp(sub_title, substr) == 0){
+	  //this is the right song
+	  memcpy(song_title, test_title, sizeof(test_title));
+	  //^ buffer overflow possible here
+	  song_title[64] = '\0';
+	  break;
+	}
+      }
+      i ++;
+    }
+    closedir(music_dir);
+    
+    //end song title loop
+
+    int song_file = open(song_title, O_RDONLY);
+    read(song_title, song, sizeof(song));
     write(socket_client, song, strlen(song) +1);
     printf("\n");
 }

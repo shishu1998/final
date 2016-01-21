a19f84baed4636d588342989403558e1a7f0821e
Jennifer Lee <jennifer.lee.ny@gmail.com>
2016-01-21 14:06:59 -0500

    login added

---
+++ 8/JEM/login.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+  printf("=======WELCOME TO MARY'S PROM DATE SERVER=======\nYou want to take Mary out to PROM because she is an awesome person and if you don't want to then you have bad taste in women.");
+  printf("
+
+}

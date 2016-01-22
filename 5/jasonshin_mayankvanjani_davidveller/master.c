1fe1dd16c1712d4b6826a0d38e15337d2f958cc2
MayanKetan Vanjani <MayanKetanVanjani@MayanKetans-iMac.local>
2016-01-21 21:15:56 -0500

    Updating master

--- 5/jasonshin_mayankvanjani_davidveller/master.c
+++ 5/jasonshin_mayankvanjani_davidveller/master.c
@@ -4,40 +4,63 @@
 #include <fcntl.h>
 #include <string.h>
 
-void map_maker(char * mapfile) {
-  char ** maparray = (char **)malloc(sizeof(char **) * 100);
+char ** map_maker(char * mapfile) {
+  char ** maparray = (char **)malloc(sizeof(char *));
+  //  char ** maparray;
   int fdMap = open("map.txt", O_RDONLY);
   char mapStr[10000];
   read(fdMap, &mapStr, sizeof(mapStr));
+
   char * p = (char *)malloc(sizeof(char *) * 100);
   p = mapStr;
-  printf("map:\n%s", mapStr);
-  int y = 0;
-  for (y = 0; p; y++) {
-    char * mapLine = (char *)malloc(sizeof(char *) * 100);
+  printf("MAP:\n%s\n", mapStr);
+
+  int i = 0;
+
+  for (i = 0; p; i++) {
+    char * mapLine = (char *)malloc(sizeof(char *));
     mapLine = strsep( &p, "\n" );
-    printf("%s",p);
+    //printf("%s",p);
     //sleep(1);
+    maparray[i] = mapLine;
+ 
+    // Next Line Prints the entire Map!!! ////////////
+    //    printf("maparray[%d]:\t%s\n",i, maparray[i]);   
+
+    /*
     
-    int x = 0;
-    for (x = 0; x < strlen(mapLine); x++) {
+      for (x = 0; x < strlen(mapLine); x++) {
+      printf("Testing\n");
       maparray[y][x] = mapLine[x];
       printf("char %d: %c\n", x, mapLine[x]);
     }
     
+    */
   }
-  return;
+  return maparray;
 }
 
-void printMap() {
+void print_map(char ** maparray) {
+  //Print the Entire 2D Array                    
+  int x, y = 0;
+  for (x = 0; x < 12; x++) {
+    for (y = 0; y < 12; y++) {
+      printf("maparray[%d][%d]:%c\n", x, y, maparray[x][y]);
+    }
+  }
+
+  /*
   int fdMap = open("map.txt", O_RDONLY);
   char mapStr[10000];  
   read(fdMap, &mapStr, sizeof(mapStr));
   printf("%s\n", mapStr);
+  */
+
   return;
 }
 
 int main() {
-  map_maker("map.txt");
+  print_map( map_maker("map.txt") );
+
   return 0;
 }

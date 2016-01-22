15edf45c07d9a61ad563fd17347c951ce256fc4e
Liiiian <ltaylor.wong@gmail.com>
2016-01-22 15:38:13 +0000

    user in

--- 4/battleship/welcome.c
+++ 4/battleship/welcome.c
@@ -30,29 +30,4 @@ int main(){
     else printf ("\nNice!\n");
 }
 
-
-
-  /*
-  command ** user_commands;
-        
-  //initial user input
-  printf("\nSlam$ ");
-  char user_input[256];
-  fgets(user_input, sizeof(user_input), stdin);  
-  //erases the new line
-  len_of_input = strlen(user_input);
-  user_input[len_of_input-1] = 0; 
-        
-  //if the user did not type exit, it keeps going 
-  while(strcmp(user_input, "exit")) {
-    parseAndRun(user_input);
-    printf("Slam$ ");
-    memset(user_input,0,sizeof(user_input));
-    //printf("USER INPUT = %s\n\n", user_input);
-    //continues getting user input
-    fgets(user_input, sizeof(user_input), stdin);
-    len_of_input = strlen(user_input);
-    user_input[len_of_input-1] = 0;
-    
-    */
     

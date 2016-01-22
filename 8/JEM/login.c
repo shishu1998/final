6c7a9d6cc748c8704524387c5a1b73e3c90e14d6
Jennifer Lee <jennifer.lee.ny@gmail.com>
2016-01-22 13:36:07 -0500

    work

--- 8/JEM/login.c
+++ 8/JEM/login.c
@@ -3,7 +3,11 @@
 #include <string.h>
 
 int main() {
+
+  char usrname[50];
   printf("=======WELCOME TO MARY'S PROM DATE SERVER=======\nYou want to take Mary out to PROM because she is an awesome person and if you don't want to then you have bad taste in women.");
-  printf("
+  printf("Please create a username:\n");
+  fgets(usrname, sizeof(usrname), stdin);
+  printf("usrname: %s\n", usrname);
 
 }

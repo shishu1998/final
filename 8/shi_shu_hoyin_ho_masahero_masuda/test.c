83e4fff7ac45fc4f3c6a7d50a100feebcc27b9e7
Shi Shu <shishu_1998@yahoo.com>
2016-01-22 13:36:12 -0500

    shuffle works

--- 8/shi_shu_hoyin_ho_masahero_masuda/test.c
+++ 8/shi_shu_hoyin_ho_masahero_masuda/test.c
@@ -3,18 +3,18 @@
 #include "deck.h"
 
 int main(){
-  card* red = (card *)malloc(sizeof(card)*746);
-  red =  makedeck("red");
+  card* red = makedeck("red");
   card* green = makedeck("green");
   // printf("content: %s\n",red[80].content);
-  printdeck(red);
   //printdeck(green);
-  //shuffle(red);
-  /* int randN;
+  //printdeck(green);
+  shuffle(green);
+  printdeck(green);
+  /*int randN;
   int counter = 30;
   while (counter){
-    randN=randNum()%getsize(red);
-    printf("Index: %d content: %s\n",randN,red[randN].content);
+    randN=randNum()%getsize(green);
+    printf("Index: %d content: %s\n",randN,green[randN].content);
     counter--;
     }*/
   //  printdeck(red);

5d727dad5ec2d7f74fdc1e0eaa7a6c17c7c021c8
TESLA <57leonardo@gmail.com>
2016-01-23 17:24:39 -0500

    anticipating change from list of client_fds to list of child_pids

--- 8/andy_tso_eric_he_leon_cheng/good-files/server.c
+++ 8/andy_tso_eric_he_leon_cheng/good-files/server.c
@@ -6,12 +6,6 @@
 #include <errno.h>
 #include "players.c"
 
-void sendpos( int fd ) {
-  char *buffer;
-  buffer = "init";
-  int n = write(fd, buffer, sizeof(buffer) );
-}
-
 char randColor( int p ) {
   char color;
   if ( p == 0 ) 
@@ -39,39 +33,56 @@ card * generateHand() {
 
 void doprocessing (int sock) {
 
-  sleep(1);
-  //printf("one (w)\n");
-  int p = write(sock, "go", sizeof("go"));
-  sleep(1); //GO TO SLEEP and wait for read to happen first
-  if (p < 0) {
-    perror("ERROR writing");
-    printf("error: %s \n", strerror(errno));
-    exit(1);
-  }
-   
-	   
-  
-  int n;
-  char buffer[256];
-  bzero(buffer,256);
-  //printf("two (r)\n");
-  n = read(sock,buffer,255);
-  //printf("able to get pass two\n");
+  //printf("current_position: %d\n", current_position);
+  //printf("desired_total: %d\n", desired_total);
 
-  if (n < 0) {
-    perror("ERROR reading from socket");
-    exit(1);
-  }
-   
-  printf("Here is the message: %s\n",buffer);
-  //printf("three (w)\n");
-  n = write(sock,"I got your message",18);
-   
-  if (n < 0) {
-    perror("ERROR writing to socket");
-    exit(1);
-  }
-	
+  /* if (current_position == desired_total){ */
+  /*   current_position = 0; */
+  /* } */
+
+  //printf("sock: %d\n", sock);  
+  //printf("player_ids[current_position]: %d\n", player_ids[current_position]);  
+  //printf("one (w)\n");
+  if (sock==player_ids[current_position]){
+    sleep(1); //GO TO SLEEP and wait for read to happen first    
+    int p = write(sock, "go", sizeof("go"));
+      
+    if (p < 0) {
+      perror("ERROR writing");
+      printf("error: %s \n", strerror(errno));
+      exit(1);
+    }
+  }	
+  
+    
+    int n;
+    char buffer[256];
+    bzero(buffer,256);
+    //printf("two (r)\n");
+    n = read(sock,buffer,255);
+    //printf("able to get pass two\n");
+
+
+    if (n < 0) {
+      perror("ERROR reading from socket");
+      exit(1);
+    }
+    
+    printf("Here is the message: %s\n",buffer);
+    //printf("three (w)\n");
+    n = write(sock,"I got your message",18);
+
+    current_position++;
+    
+    if (n < 0) {
+      perror("ERROR writing to socket");
+      exit(1);
+    }
+
+  //printf("current_position: %d\n", current_position);
+  //printf("desired_total: %d\n", desired_total);
+  
+  
 }
 
 int main( int argc, char *argv[] ) {
@@ -121,16 +132,17 @@ int main( int argc, char *argv[] ) {
     if(player_count <= desired_total){
       
       newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-      
+
       //if ( newsockfd == 4 ) 
       //  sendpos( 4 );
       
       players_connect();
+
       player_ids[player_count-1] = newsockfd;
       int i=0;
       while(i<player_count){
-	printf("player_ids[%d] = %d\n", i, player_ids[i]);
-	i++;
+      	printf("player_ids[%d] = %d\n", i, player_ids[i]);
+      	i++;
       }
       
       if (newsockfd < 0) {
@@ -147,21 +159,40 @@ int main( int argc, char *argv[] ) {
       }
     
       if (pid == 0) {
+	
+	printf("pid: %d\n", getpid());
+	
 	while( 1 ) {		
 	  /* This is the client process */
 	  printf("hello\n");
 	  //close(sockfd);
 	  doprocessing(newsockfd);
+
+	  printf("current_position: %d\n", current_position);
+	  printf("desired_total: %d\n", desired_total);
+	  printf("player_ids[current_position]: %d\n", player_ids[current_position]);
+	  int i=0;
+	  while(i<player_count){
+	    printf("player_ids[%d] = %d\n", i, player_ids[i]);
+	    i++;
+	  }
+	  printf("current_position: %d\n", current_position);
+	  printf("newsockfd: %d\n", newsockfd);
+	  //sleep(1);
+	  
 	}
+
       }
     }
     else {
+      /* Close the client if desired_total num of players aleady connected */
       write(newsockfd, "terminate", sizeof("terminate"));
       close(newsockfd);
-      if (pid ==0){
+      if (pid==0){
 	close(sockfd);
       }
     }
 		
   } /* end of while */
+
 }

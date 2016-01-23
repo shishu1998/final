dd00e1eddcdca0818bbc0089eb845fb67e3ecaca
margarthebar <margarburkart@gmail.com>
2016-01-23 12:34:50 -0500

    finished sign_in

--- 5/margaretburkart_aidanfitzgerald/client.c
+++ 5/margaretburkart_aidanfitzgerald/client.c
@@ -13,6 +13,27 @@ void strip_add(char* source, char* dest){
 }
 
 void sign_in(int socket_id){
+  char use[256];
+  char pass[256];
+  char final[256];
+  char* use_phrase = "LOGIN\nUsername: ";
+  char* pass_phrase = "\nPassword: ";
+
+  strcat(final,use_phrase);
+  printf("Enter your username: ");
+  fgets(use,MAXLEN,stdin);
+  strip_add(use,final);
+
+  strcat(final,pass_phrase);
+  printf("Enter your password: ");
+  fgets(pass,MAXLEN,stdin);
+  strip_add(pass,final);
+
+  sock_write(socket_id,final);
+  printf("This is what you sent to the server:\n[%s]\n",final);
+
+
+  /*
   char buff[256];
   char *buffer = buff;
   char buff1[256];
@@ -32,25 +53,13 @@ void sign_in(int socket_id){
 
   //write(socket_id, final, sizeof(final) -1);
   sock_write(socket_id,final);
+  */
 }
 
 void choose_username(char* final, char* use){
   printf("Enter your new username: ");
   fgets(use,MAXLEN,stdin);
   strip_add(use,final);
-  /*
-  printf("[%s] %lu\n",final,strlen(final));
-  printf("Enter your new username: ");
-  fgets(buffer,sizeof(stdin)-1,stdin);
-  printf("Successfully read from stdin\n");
-  strncat(final,buffer,12);
-  printf("[%s] %lu\n",final,strlen(final));
-  printf("Successfully used strcat\n");
-  
-  char *sep = "\nPassword: ";
-  strcat(final,sep);
-  printf("[%s] %lu\n",final,strlen(final));
-  */
 }
 
 void choose_password(char* final, char* pass, char* pass2, int socket_id){
@@ -66,24 +75,6 @@ void choose_password(char* final, char* pass, char* pass2, int socket_id){
     printf("Passwords did not match. Try again:\n");
     choose_password(final, pass, pass2, socket_id);
   }
-
-  /*
-  printf("Enter your new password: ");
-  fgets(buffer1,sizeof(stdin)-1,stdin);
-  printf("\n[%s] %lu\n",final,strlen(final));
-  printf("Re-enter your new password: ");
-  fgets(buffer2,sizeof(stdin)-1,stdin);
-  printf("\n[%s] %lu\n",final,strlen(final));
-
-  if(strcmp(buffer1,buffer2)==0){
-    strncat(final,buffer2,12);
-    //write(socket_id, final, sizeof(final) -1);
-    sock_write(socket_id,final);
-  }else{
-    printf("Passwords did not match. Try again:\n");
-    choose_password(final, buffer1, buffer2, socket_id);
-  }
-  */
 }
 
 void sign_up(int socket_id){
@@ -108,35 +99,6 @@ void sign_up(int socket_id){
 
     printf("This is what you sent to the server:\n[%s]\n",final);
   }
-
-  /*
-  char buff[512];
-  char *buffer = buff;
-  char buff1[512];
-  char *buffer1 = buff;
-  char buff2[512];
-  char *buffer2 = buff;
-  char fin[2048] = "SETUP\nUsername: ";
-  char *final;
-  final = fin;
-
-  int i;
-  i = fork();
-  if(i==-1){
-    //return errno;
-  }else if(i==0){
-    execl("mkdir","mkdir","mail");
-    //return 0;
-  }else{
-    //int exit;
-    //int pid = wait(&exit);
-    //return WEXITSTATUS(exit);
-    //choose_username(final, buffer);
-    //choose_password(final, buffer1, buffer2, socket_id);
-    choose_username(final, buffer);
-    choose_password(final, buffer1, buffer2, socket_id);
-  }
-  */
 }
 
 void check_for_account(char *buffer, int socket_id){

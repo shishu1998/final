5ef52609e69684ea755c085ea2b130e1d52ba689
Jennifer Lee <jennifer.lee.ny@gmail.com>
2016-01-23 21:26:34 -0500

    login problems

--- 8/JEM/login.c
+++ 8/JEM/login.c
@@ -16,17 +16,17 @@ void find_username(char *username) {
 }
 
 int main() {
-  char *usrname;
-  char *newline;
+  void *usrname;
+  //char *newline;
   char *yes_no;
   char *one = (char *)malloc(3*sizeof(char *));
   char *two = (char *)malloc(3*sizeof(char *));
-  newline = (char *)malloc(sizeof(char *));
-  usrname = (char *)malloc(50*sizeof(char));
+  //newline = (char *)malloc(sizeof(char *));
+  usrname = malloc(50*sizeof(char));
   printf("sizeof(usrname) = %lu\n", sizeof(usrname));
 
   yes_no = (char *)malloc(sizeof(char *));
-  newline = "\n";
+  //newline = "\n";
   one ="1\0"; two = "2\0";
 
   printf("=======WELCOME TO MARY'S PROM DATE SERVER=======\nYou want to take Mary out to PROM because she is an awesome person and if you don't want to then you have bad taste in women.\n\nPlease type in 1 if you have an account or 2 if you'd like to make one.\n");
@@ -40,19 +40,20 @@ int main() {
     printf("username without new line: %s\n", usrname);
     char *buffer = (char *)malloc(1024*sizeof(char));
     //printf("sizeof(buffer) = %lu\n", sizeof(*buffer));
-    fread(buffer, 1, 1024, fd1);
+    fread(buffer, sizeof(char), 1024, fd1);
     printf("buffer: %s\n", buffer);
-    
+    buffer = "";
+    printf("buffer after fread/fwrite: %s\n", buffer);    
   }
   else if (strncmp(yes_no, two, 1) == 0){
     FILE* fd1 = fopen("username.txt", "a+");
     printf("Please create a username:\n");
-    fgets(usrname, 100, stdin);
+    fgets(usrname, sizeof(usrname), stdin);
     printf("username without new line: %s\n", usrname);
     //strcat(usrname, newline);
     //printf("username WITH new line: %sword\n", usrname);
     //printf("sizeof(usrname) = %lu\n", sizeof(usrname));
-    fwrite(usrname, 1, 50, fd1);
+    fwrite(usrname, sizeof(char), 50, fd1);
         
     fclose(fd1);
     return 0;

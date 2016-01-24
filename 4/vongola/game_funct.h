705d08e6bc7b5e10008606ba3876e4ab986509a6
tonyli7 <xxtonyxxlixx@gmail.com>
2016-01-24 03:16:26 -0500

    more globals

--- 4/vongola/game_funct.h
+++ 4/vongola/game_funct.h
@@ -1,12 +1,23 @@
+//--------ROLES----------
 #define TOWNIE 0
 #define MAFIASO 1
 #define DOCTOR 2
 #define COP 3
 
+//-------STATUS----------
+#define DEAD 0
+#define ALIVE 1
+
+//-------VOTE------------
+#define NO 0
+#define YES 1
+
 typedef struct player{
   char name[256];
   int role;
   int fd;
+  int status;
+  int vote;
 }
   player;
 

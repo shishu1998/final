46e4204af61eeeeb7a5a608fd307323a334b5949
Jennifer Lee <jennifer.lee.ny@gmail.com>
2016-01-24 12:41:40 -0500

    password

--- 8/JEM/login.c
+++ 8/JEM/login.c
@@ -5,6 +5,9 @@
 #include <unistd.h>
 #include <errno.h>
 
+#define USER_LEN 20
+#define PSWD_LEN 20
+
 void make_profile(char *username) {
 
 }
@@ -16,29 +19,30 @@ void find_username(char *username) {
 }
 
 int main() {
-  char input[5];
-  char user[20];
+  char user[USER_LEN]; char pswd[PSWD_LEN];
+  char underscore[1] = "_";
   //char *newline;
-  char *username;
-  char *yes_no;
-  char *one = (char *)malloc(3*sizeof(char *));
-  char *two = (char *)malloc(3*sizeof(char *));
+  char *username;// char *password;
+  char yes_no;
   //newline = (char *)malloc(sizeof(char *));
-  //usrname = malloc(50*sizeof(char));
-  yes_no = (char *)malloc(sizeof(char *));
   //newline = "\n";
-  one ="1\0"; two = "2\0";
 
   printf("=======WELCOME TO MARY'S PROM DATE SERVER=======\nYou want to take Mary out to PROM because she is an awesome person and if you don't want to then you have bad taste in women.\n\nPlease type in 1 if you have an account or 2 if you'd like to make one.\n");
-  fgets(yes_no, 3, stdin);
+  fgets(&yes_no, 3, stdin);
   
-  printf("yes_no value:%s\n", yes_no);
+  printf("yes_no value:%c\n", yes_no);
   
-  if (strncmp(yes_no, one, 1) == 0) {
+  if (yes_no == '1') {
     FILE* fd1 = fopen("username.txt", "r");
     printf("Please type in your username:\n");
-    fgets(user, sizeof(user), stdin);
-    printf("user without new line: %s\n", user);
+    fgets(user, USER_LEN, stdin);
+
+    printf("Please type in your password:\n");
+    fgets(pswd, PSWD_LEN, stdin);
+
+    printf("user: %s\n", user);
+    printf("pswd: %s\n", pswd);
+
     char *buffer = (char *)malloc(100*sizeof(char));
     //printf("sizeof(buffer) = %lu\n", sizeof(buffer));
     fread(buffer, sizeof(char), 100, fd1);
@@ -46,18 +50,29 @@ int main() {
     buffer = "";
     printf("buffer after fread/fwrite: %s\n", buffer);    
   }
-  else if (strncmp(yes_no, two, 1) == 0){
+
+  else if (yes_no == '2'){
     FILE* fd1 = fopen("username.txt", "a+");
     printf("Please create a username:\n");
-    fgets(user, 10, stdin);
-    printf("username array: %s\n", user);
-    username = calloc(strlen(user) + 1, sizeof(char));
+    fgets(user, USER_LEN, stdin);
+    
+    printf("Please create a password:\n");
+    fgets(pswd, PSWD_LEN, stdin);
+    //printf("username array: %s\n", user);
+    //printf("pswd array: %s\n", pswd);
+    username = calloc(strlen(user) + strlen(pswd) + 1 + 1, sizeof(char));//1 is for the underscore and the other is for the null char
+    //password = calloc(strlen(pswd) + 1, sizeof(char));
+
     strcat(username, user);
-    
-    printf("sizeof(username) = %lu\n", sizeof(username));
+    strsep(&username, "\n");
+    printf("sizeof(username) = %lu\n", strlen(username));
     printf("username: %s\n", username);
-    
-    fwrite(username, sizeof(char), sizeof(username), fd1);
+
+    strcat(username, underscore);
+    printf("username: %s\n", username);
+    //strcat(password, pswd);
+    strcat(username, pswd);
+    fwrite(username, sizeof(char), strlen(username), fd1);
 
         
     fclose(fd1);

c4ae0362b790c0579dccedabb686c5b7950db062
turtle_inaho <eriche864@gmail.com>
2016-01-24 23:55:43 +0000

    working with top card

--- 8/andy_tso_eric_he_leon_cheng/after-deviation/client.c
+++ 8/andy_tso_eric_he_leon_cheng/after-deviation/client.c
@@ -20,11 +20,50 @@ static void sighandler(int signo) {
   }
 }
 
+int count_tokens( char *line, char *delim ) {
+  int tokens = 0;
+  char *dup = strdup( line );
+  while ( dup ) {
+    //char *del = (char *)delim;
+    strsep( &dup, delim );
+    tokens++;
+  }
+  return tokens;
+}
+
+/*======== char ** parse_line() ==========
+  Inputs:  char *line 
+  Returns: Array of strings where each entry is a token 
+  separated by delim
+  If line contains multiple tokens separated by delim, this 
+  function will put each token into an array of strings
+  ====================*/
+
+char **parse_line( char *line, char *delim ) {
+  
+  int i;
+  char *dup = strdup( line );
+  int len = count_tokens( dup, delim );
+  //printf( "len[%d] \n", len );
+  char **array_tokens = malloc( sizeof(*array_tokens) * len+1 );
+  char *token;
+  if ( array_tokens ) {
+    for ( i = 0; i < len; i++ ) {
+      token = strsep( &line, delim );
+      array_tokens[i] = malloc( sizeof( *array_tokens[i] * 11 ) );
+      strcpy( array_tokens[i], token );
+      //printf( "array_tokens: %s\n", token );
+    }
+  }
+  array_tokens[i++] = NULL;
+  return array_tokens;
+}
+
 int main(int argc, char *argv[]) {
 
   p = generate_hand(p);
   printf("p.num_cards: %d\n", p.num_cards);
-  player_action(p);
+  //player_action(p);
   
   signal(SIGINT, sighandler);
 
@@ -67,15 +106,18 @@ int main(int argc, char *argv[]) {
   }
 
   while (1) {
+    
+    player_action(p);
 
     /* Now ask for a message from the user, this message
      * will be read by server
      */
 
     /* Server determines turn */
-    char buffer2[256];
+    char *buffer2;
     //printf("four (r)\n");
-    int a = read(sockfd, buffer2, 255);
+    int a = read(sockfd, buffer2, 255 );
+    //printf ( "debug\n" );
     //printf("able to get pass four\n");
     if (a < 0) {
       perror("ERROR reading");
@@ -95,27 +137,34 @@ int main(int argc, char *argv[]) {
 
       char * scard;
       int num = atoi(buffer);
+      printf( "debugging\n");
       if (num==p.num_cards){
-	p.cards[p.num_cards] = draw_card();
-	p.num_cards++;
+        printf( "debugging\n");
+	      p.cards[p.num_cards] = draw_card();
+	      printf( "debugging\n");
+	      p.num_cards++;
+	      printf( "debugging\n");
+	      scard = "draw";
       }
       else{
-	card c = p.cards[num];
-	int value = c.value;
-	int color = c.color;
-	char svalue[100];
-	sprintf(svalue, "%d", value);
-	char cvalue[100];
-	sprintf(cvalue, "%d", color);	
-	scard = strcat(svalue, ",");
-	scard = strcat(scard, cvalue);
-	printf("tried to stringify a card in client: %s \n", scard);
+      	card c = p.cards[num];
+      	int value = c.value;
+      	int color = c.color;
+      	char svalue[100];
+      	sprintf(svalue, "%d", value);
+      	char cvalue[100];
+      	sprintf(cvalue, "%d", color);	
+      	scard = strcat(svalue, ",");
+      	scard = strcat(scard, cvalue);
+      	p = remove_card(p, num);
+      	printf("tried to stringify a card in client: %s \n", scard);
       }
 
       /* Send message to the server */
       //printf("five (w)\n");
       //n = write(sockfd, buffer, strlen(buffer));
       n = write(sockfd, scard, strlen(scard));
+      //char **crd = parse_line( scard, "," );
     }
     if (n < 0) {
       perror("ERROR writing to socket");

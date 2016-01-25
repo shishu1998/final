c8a6c24644b8485d61e09f42391347c9b69288f2
Thomas Wong <twong3.1415926@gmail.com>
2016-01-25 06:40:12 -0500

    helper functions done, turn in progress
    Former-commit-id: 3518c9b4dd65f2d0580aedaad12ff18ab54c7d27

--- 5/jared_beh_michael_rivera_thomas_wong/game.h
+++ 5/jared_beh_michael_rivera_thomas_wong/game.h
@@ -1,3 +1,6 @@
+#IFNDEF GAME_H
+#DEFINE GAME_H
+
 typedef struct {
 
   char mini_board[3][3];  // Each board is 3x3 board!
@@ -6,10 +9,12 @@ typedef struct {
 
 } board;
 
-board super_board[3][3];
+extern board super_board[3][3];
 
 void board_init();            // initialize empty board
-int turn( char R, char C, char r, char c);    // react to legal or illegal player input
-char check_super_win();  // check to see if the super_board is won
+int turn( int R, int C, int r, int c);    // react to legal or illegal player input
+char check_win();  // check to see if the super_board is won
 void update_winner( board );  // update winner variable
 void update_full( board );    // update full variable
+
+#ENDIF

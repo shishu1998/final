eac62aeafb8026548bd7cc354943602105646368
dennisYatunin <dyatun@gmail.com>
2016-01-25 16:27:23 -0500

    Oh say can you see
    Former-commit-id: 9c99abaf2220dad9d2629fee0a36dffb8d0b9cf5

--- 8/dennis-yatunin_max-fishelson/connections.c
+++ 8/dennis-yatunin_max-fishelson/connections.c
@@ -2,41 +2,72 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
+#include <errno.h>
 
-#typedef struct name_or_ip {
+#define PORT_STR "8000"
+#define PORT_INT 8000
 
-};
+typedef struct ip_or_name {
+	int is_ip;
+	char *ip;
+	char *hostname;
+} ip_or_name;
 
 void error(char *msg) {
 	perror(msg);
 	exit(EXIT_FAILURE);
 }
 
-// Usage: client("localhost", "8000")
-// service = port number or service name
-int client(char *hostname, char *service) {
-	struct addrinfo hints, *res;
-	int err, sock;
+int open_client_socket(ip_or_name *ion) {
+	int sock;
 
-	memset(&hints, 0, sizeof(hints));
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_family = AF_INET;
+	if (ion->is_ip) {
+		struct sockaddr_in server;
 
-	if ((err = getaddrinfo(hostname, service, &hints, &res)) != 0) {
-		printf("getaddrinfo: %s\n", gai_strerror(err));
-		return 1;
+		sock = socket(AF_INET , SOCK_STREAM , 0);
+		if (sock == -1) {
+			error("socket");
+		}
+
+		server.sin_addr.s_addr = inet_addr(ion->ip);
+		server.sin_family = AF_INET;
+		server.sin_port = htons(PORT_INT);
+
+		if (connect(sock, (struct sockaddr *)&server , sizeof(server)) == -1) {
+			if (errno == ECONNREFUSED) {
+				errno = 0;
+				return 0;
+			}
+			error("connect");
+		}
 	}
 
-	sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-	if (sock < 0) {
-		error("socket");
+	else {
+		struct addrinfo hints, *res;
+		int err;
+
+		memset(&hints, 0, sizeof(hints));
+		hints.ai_socktype = SOCK_STREAM;
+		hints.ai_family = AF_INET;
+
+		if ((err = getaddrinfo(ion->hostname, PORT_STR, &hints, &res)) != 0) {
+			printf("getaddrinfo: %s\n", gai_strerror(err));
+			return 1;
+		}
+
+		sock = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+		if (sock == -1) {
+			error("socket");
+		}
+
+		if (connect(sock, res->ai_addr, res->ai_addrlen) == -1) {
+			if (errno == ECONNREFUSED) {
+				errno = 0;
+				return 0;
+			}
+			error("connect");
+		}
 	}
 
-	if (connect(sock, res->ai_addr, res->ai_addrlen) != 0) {
-		error("connect");
-	}
-
-	freeaddrinfo(res);
-
-	return 0;
+	return sock;
 }

cfb20a8a037873f01e910124e174b72c60138519
Jennifer Lee <jennifer.lee.ny@gmail.com>
2016-01-25 15:34:58 -0500

    underscore duplicate

--- 8/JEM/Single Server/login.h
+++ 8/JEM/Single Server/login.h
@@ -1,7 +1,11 @@
 const char underscore = '_';
 
+#define USER_LEN 20                                                                                                                                                     
+#define PSWD_LEN 20                                                                                                                                                               
+#define BUFFER_LEN 500     
+
 int find_error(char*, char*);
 
 int find_user_match(char*);
 
-void find_user();
+int find_user();

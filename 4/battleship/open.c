85d1c5c1e5fd9b186f9fc088b00cfaa65674c6dd
Liiiian <ltaylor.wong@gmail.com>
2016-01-25 05:36:43 +0000

    stuff
    Former-commit-id: 9a21c20418118d40839de764619e7ed92946006f

--- 4/battleship/open.c
+++ 4/battleship/open.c
@@ -72,12 +72,12 @@ int main(){
   printf("OO\n");
   printf("OO\n\n");
   sleep(1);
-  printf("                             OOOOOO     OOOO    OOOOOO   OOOOOO   OO       OOOOOO    OOOOOO   OO  OO   OO   OOOOO\n");
-  printf("                             OO   OO   OO  OO     OO       OO     OO       OO        OO       OO  OO   OO   OO  O\n");
-  printf("                             OO  OO    OO  OO     OO       OO     OO       OO        OO       OOOOOO   OO   OO  O\n");
-  printf("                             OOOOOO    OOOOOO     OO       OO     OO       OOOO      OOOOOO   OOOOOO   OO   OOOO\n");
-  printf("                             OO   OO   OO  OO     OO       OO     OO       OO           OOO   OO  OO   OO   OO \n");
-  printf("                             OOOOOO    OO  OO     OO       OO     OOOOOO   OOOOOO    OOOOOO   OO  OO   OO   OO\n\n");
+  printf("                           OOOOOO     OOOO    OOOOOO   OOOOOO   OO       OOOOOO    OOOOOO   OO  OO   OO   OOOOO\n");
+  printf("                           OO   OO   OO  OO     OO       OO     OO       OO        OO       OO  OO   OO   OO  O\n");
+  printf("                           OO  OO    OO  OO     OO       OO     OO       OO        OO       OOOOOO   OO   OO  O\n");
+  printf("                           OOOOOO    OOOOOO     OO       OO     OO       OOOO      OOOOOO   OOOOOO   OO   OOOO\n");
+  printf("                           OO   OO   OO  OO     OO       OO     OO       OO           OOO   OO  OO   OO   OO \n");
+  printf("                           OOOOOO    OO  OO     OO       OO     OOOOOO   OOOOOO    OOOOOO   OO  OO   OO   OO\n\n");
   
   
   

851c7074b91a36533368308de57ac10bda89b193
Samuel Zhang <samuelzhang3@gmail.com>
2016-01-26 02:15:19 -0500

    makasa

---
+++ 8/katherine_gershfeld_mariya_gedrich_samuel_zhang/client.h
@@ -0,0 +1,16 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include "player.h"
+
+#endif

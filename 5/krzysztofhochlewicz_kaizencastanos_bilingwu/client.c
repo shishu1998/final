231305bdd73ba7307a7b4e38420c94b224378e49
bilingwu <sarawu45@gmail.com>
2016-01-26 08:09:54 -0500

    updating client and server

--- 5/krzysztofhochlewicz_kaizencastanos_bilingwu/client.c
+++ 5/krzysztofhochlewicz_kaizencastanos_bilingwu/client.c
@@ -11,9 +11,8 @@
 
 int main(int argc, char **argv){
   
-  int socket_id;
+  int socket_id, errno,i;
   char buffer[256];
-  int i;
 
   //creating the socket 
   socket_id = socket(AF_INET, SOCK_STREAM, 0);
@@ -25,12 +24,23 @@ int main(int argc, char **argv){
   inet_aton("127.0.0.1", &(sock.sin_addr));
   bind(socket_id, (struct sockaddr *)&sock, sizeof(sock));
   
+  
   //attempt to make a connection
   i = connect(socket_id, (struct sockaddr*)&sock, sizeof(sock));
-  printf("<client> connect returned: %d\n", i);
-
-  read(socket_id, buffer, sizeof(buffer));
-  printf("<client> recieved: [%s]\n", buffer);
+  if (i == -1) {
+    printf("<client>Error connecting to server: %s\n", strerror(errno));
+    return 0;
+  }
+  else{
+    printf("<client> Connection made to %d\n", socket_id);
+  }
+  while (1){
+    printf("Enter Message:");
+    fgets(buffer, 256, stdin); //replace this buffer with some type of int to send to server
+    send(socket_id, buffer, sizeof(buffer), 0);
+    recv(socket_id, buffer, sizeof(buffer), 0);
+    printf("<client> received [%s]\n\n", buffer);
+  }
 
   return 0;
 }

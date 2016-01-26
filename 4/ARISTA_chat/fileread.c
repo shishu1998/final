fc48de0051bb169c20e0f6dcfb437b55f6e6b205
Your Name <caitlinstanton44@gmail.com>
2016-01-25 21:03:59 -0500

    filereading function

--- 4/ARISTA_chat/fileread.c
+++ 4/ARISTA_chat/fileread.c
@@ -1,27 +1,62 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
-#include <dirent.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <errno.h>
-#include <unistd.h>
-#include <fcntl.h>
 
-int main()
-{
-    FILE *fp=fopen("tutoraccounts.txt","r");
-    char tmp[256];
-    strcpy(tmp,"hello");
-    while(fp!=NULL && fgets(tmp, sizeof(tmp), fp)!=NULL)
-    {
-        if (strstr(tmp, "word1")) {
-            printf("%s", tmp);
-	} else if (strstr(tmp, "word2")) {
-            printf("%s", tmp);
+void Usage(char *filename) {
+	printf("Usage: %s <file> <string>\n", filename);
+	printf("%s version 1.0 \nCopyright(c) CodingUnit.com\n", filename);
+}
+
+int Search_in_File(char *fname, char *str) {
+	FILE *fp;
+	int line_num = 1;
+	int find_result = 0;
+	char temp[512];
+	
+	//gcc users
+	if((fp = fopen(fname, "r")) == NULL) {
+		return(-1);
 	}
-    }
-    if(fp!=NULL) fclose(fp);
-    return 0;
+
+while(fgets(temp, 512, fp) != NULL) {
+		if((strstr(temp, str)) != NULL) {
+			printf("A match found on line: %d\n", line_num);
+			printf("\n%s\n", temp);
+			find_result++;
+		}
+		line_num++;
+	}
+
+	if(find_result == 0) {
+		printf("\nSorry, couldn't find a match.\n");
+	}
+	
+	//Close the file if still open.
+	if(fp) {
+		fclose(fp);
+	}
+   	return(0);
+}
+
+
+int main(int argc, char *argv[]) {
+	int result, errno;
+
+	if(argc < 3 || argc > 3) {
+		Usage(argv[0]);
+		exit(1);
+	}
+
+	//Use system("cls") on windows
+	//Use system("clear") on Unix/Linux
+	system("clear");
+
+	result = Search_in_File(argv[1], argv[2]);
+	if(result == -1) {
+		perror("Error");
+		printf("Error number = %d\n", errno);
+		exit(1);
+	}
+	return(0);
 }
 

30b367c6d79b2ed090efcdd444377e14923bc43a
Dillon Zhang <d1998z@gmail.com>
2016-01-26 05:44:45 -0500

    FINISHED

--- 8/angela_chan_dillon_zhang/manual.c
+++ 8/angela_chan_dillon_zhang/manual.c
@@ -19,7 +19,8 @@ int main() {
   int age = fork();
   int information;
   int waiting;
-  
+
+  //Forks in order to cat the cover page
   if (age) {
     waiting = wait(&information);
     printf("\nEnter 'next' to continue: ");
@@ -27,22 +28,28 @@ int main() {
     execlp("cat", "cat", "asciiart/manual/mancover.dat", NULL);
   }
 
+  //Counting the pages
   int pagecounter = -1;
 
   while(1) {
+    //Takes in user input and remembers when pages have to be updated
     char user_input[64];
     int new_page = 0;
-    
+
+    //Take in user inputs
     fgets(user_input, sizeof(user_input), stdin);
     *strchr(user_input, '\n') = 0;
 
+    //Do they want to leave
     if (!strcmp(user_input,"exit")) {
       textcolor(BRIGHT, RED, BLACK);
       printf("Goodbye! Thanks for playing!");
       textcolor(RESET, WHITE, BLACK);
       exit(0);
+
+      //Do they want to go to the next page and is there a page there.
     } else if (!strcmp(user_input,"next")) {
-      if (pagecounter < 100 /*Length of Manual*/) {
+      if (pagecounter < 14 /*Length of Manual*/) {
 	pagecounter++;
 	new_page = 1;
       } else {
@@ -50,6 +57,8 @@ int main() {
 	printf("Sorry you have reached the end of the manual.");
 	textcolor(RESET, WHITE, BLACK);
       }
+
+      //Do they want to go to the previous page and is there a page there.
     } else if (!strcmp(user_input,"back")) {
       if (pagecounter > 0) {
 	pagecounter--;
@@ -59,12 +68,15 @@ int main() {
 	printf("Sorry you have reached the beginning of the manual.");
 	textcolor(RESET, WHITE, BLACK);
       }
+
+      //Did the done goof?
     } else {
       textcolor(BRIGHT, RED, BLACK);
       printf("Sorry, %s is not a valid command.", user_input);
       textcolor(RESET, WHITE, BLACK);
     }
 
+    //Clearing the terminal window and showing the next page with the prompt
     if (new_page) {
       new_page = 0;
       printf("\e[1;1H\e[2J\n");

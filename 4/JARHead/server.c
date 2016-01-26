c0c1e1ac40fb3dee1ca9b6e22fd1418f3bedf900
Alvin Lin <alvin.lin.dev@gmail.com>
2016-01-25 21:44:46 -0500

    map initializers

--- 4/JARHead/server.c
+++ 4/JARHead/server.c
@@ -11,14 +11,15 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+#include "map.h"
+
 #define PORT 5000
 #define ID 0
-#define MEM_SIZE 1024
 
 static void clean_up_memory(int signo) {
   int e, sd, key;
   key = ftok("Makefile", ID);
-  sd = shmget(key, MEM_SIZE, 0644);
+  sd = shmget(key, MAP_MEMORY_SIZE, 0644);
   e = shmctl(sd, IPC_RMID, NULL);
   printf("Shared memory removed: %d\n", e);
   exit(0);
@@ -39,8 +40,13 @@ int main() {
 
   // Create shared memory segment.
   key = ftok("Makefile", ID);
-  sd = shmget(key, 1024, 0644 | IPC_CREAT);
+  sd = shmget(key, MAP_MEMORY_SIZE, 0644 | IPC_CREAT);
   printf("Shared memory created: %d\n", sd);
+
+  // Initialize the map
+  map = shmat(sd, 0, 0);
+  init(map);
+  display_printf(map);
 
   // create the socket
   socket_id = socket( AF_INET, SOCK_STREAM, 0 );

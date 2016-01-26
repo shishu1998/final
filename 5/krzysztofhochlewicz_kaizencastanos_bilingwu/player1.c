8ee61d7f524ce212239a1c8c0a21d9ae4cdbcb9d
bilingwu <sarawu45@gmail.com>
2016-01-26 09:05:08 -0500

    one more bug to freedom

--- 5/krzysztofhochlewicz_kaizencastanos_bilingwu/player1.c
+++ 5/krzysztofhochlewicz_kaizencastanos_bilingwu/player1.c
@@ -37,7 +37,7 @@ char *rwp = remaining_words; //frustrating C string stuff that is necessary for 
 const char *delim = " ";
 char current_status[1024];
 
-int socket_id, socket_client;
+int socket_id, socket_client,pid;
 char buffer[256];
 
 void update_screen();
@@ -48,7 +48,7 @@ int getData(char* str);
 int main(){
   
   //--------------------//
-  //----Server Stuff----//
+  //----Network Stuff----//
   //--------------------//
   
   //create the socket
@@ -68,7 +68,7 @@ int main(){
   printf("<server> connected: %d\n", socket_client);
   
   //write(socket_client, "stuff", 6);
-  printf("Game Starting Soon!\n");
+  printf("Get Ready! Game Starting Soon!\n");
   sleep(4);
   printf("Game starting in three--\n");
   sleep(1);
@@ -81,91 +81,118 @@ int main(){
   //----The Actual Game Bit----//
   //---------------------------//
   clock_t start = clock(), diff;
-	printf("\033[2J\033[1;1H"); //clear the screen
-	printf("%s\n", game_header);
-	printf("\n");
-	printf("%s\n", remaining_words);
+  printf("\033[2J\033[1;1H"); //clear the screen
+  printf("%s\n", game_header);
+  printf("\n");
+  printf("%s\n", remaining_words);
 	
-	current_word = strsep(&rwp, delim); //get the first word
+  current_word = strsep(&rwp, delim); //get the first word
 
-	int check_1 = 0; //Used to fix a bug where a space kept appearing after you typed a word
-	int check_2 = 1; //To keep the first check from affecting the first word
-	while(current_word != NULL){ 				 //loop over words until there are no more words
-		entered_chars = 0;
-		current_typed[0] = 0;
-		if(check_2 != 1)
-			check_1 = 1;
+  int check_1 = 0; //Used to fix a bug where a space kept appearing after you typed a word
+  int check_2 = 1; //To keep the first check from affecting the first word
+  while(current_word != NULL){
+    //loop over words until there are no more words
+    entered_chars = 0;
+    current_typed[0] = 0;
+    if(check_2 != 1)
+      check_1 = 1;
 			
-		while(strcmp(current_word, current_typed)){ //loop until the user enters the current word correctly
-			current_char = get_letter(); //wait for a key to be pressed
-			if (current_char ==  127){   //if backspace pressed
-				if (entered_chars > 0){  //if there are entered chars for the current word
-					entered_chars--;
-					current_typed[strlen(current_typed)-1] = 0; //get rid of last letter
-				}
-			}
-			else{ //if not a backspace
-				entered_chars++;
-				current_typed[strlen(current_typed)+1] = 0;
-				current_typed[strlen(current_typed)] = current_char;
-			}
-			printf("\033[2J\033[1;1H"); //clear the screen
-			update_screen(); //update status
-			
-			if(check_1)
-				current_typed[0] = 0;
-			check_1 = 0;
-		}
-		
-		strcat(completed_words, current_word);
-		strcat(completed_words, " ");
-		current_word = strsep(&rwp, delim);
-		check_2 = 0;
-		number_of_words++;
+    while(strcmp(current_word, current_typed)){ //loop until the user enters the current word correctly
+      current_char = get_letter(); //wait for a key to be pressed
+      if (current_char ==  127){   //if backspace pressed
+	if (entered_chars > 0){  //if there are entered chars for the current word
+	  entered_chars--;
+	  current_typed[strlen(current_typed)-1] = 0; //get rid of last letter
 	}
+      }
+      else{ //if not a backspace
+	entered_chars++;
+	current_typed[strlen(current_typed)+1] = 0;
+	current_typed[strlen(current_typed)] = current_char;
+      }
+      printf("\033[2J\033[1;1H"); //clear the screen
+      update_screen(); //update status
+			
+      if(check_1)
+	current_typed[0] = 0;
+      check_1 = 0;
+    }
+		
+    strcat(completed_words, current_word);
+    strcat(completed_words, " ");
+    current_word = strsep(&rwp, delim);
+    check_2 = 0;
+    number_of_words++;
+    
+    //THUS BEGINS SENDING INFORMATION TO PLAYER2 (CLIENT)
+    //MAY THE ODDS BE EVER IN MY FAVOR
+    int converted_number = htonl(number_of_words);
+      
+      /*
+      recv(socket_client, buffer, sizeof(buffer), 0);
+      printf("<server> Recieved:[%s]\n", buffer);
+      strtok(buffer, "\n");
+      send(socket_client, number_of_words, sizeof(number_of_words), 0); //FOR INT STUFFS WE JUST HAVE TO GET THAT AS A VARIABLE AND FUCKING SEND THAT SHIT HERE
+      */
+    write(socket_client, &converted_number, sizeof(converted_number));
+  
+    int received_int = 0;
+    int return_status;
+    return_status = read(socket_client, &received_int, sizeof(received_int));
+    if (return_status > 0) {
+      fprintf(stdout, "Received int = %d\n", ntohl(received_int));
+    }
+    else {
+      printf("<client>Failed to read");
+    }
+    
+    //if ever the need to comment new stuff out, end it here
+  }
 
-	//Return the time
-	diff = clock() - start;
-	int msec = diff * 1000 / CLOCKS_PER_SEC;
-	printf("\nTime taken: %d seconds %d milliseconds\n", msec/1000, msec%1000); //Exact time
-	printf("Words per minute: %f WPM\n", (13.0/(msec/1000))* 60); //words divided by seconds, multiplied by 60 seconds
+  //Return the time
+  diff = clock() - start;
+  int msec = diff * 1000 / CLOCKS_PER_SEC;
+  printf("\nTime taken: %d seconds %d milliseconds\n", msec/1000, msec%1000); //Exact time
+  printf("Words per minute: %f WPM\n", (13.0/(msec/1000))* 60); //words divided by seconds, multiplied by 60 seconds
 
 
 
   return 0;
 }
+
+
 int a = 0;
 void update_screen(){
   /*write(socket_client, makeData(), 6);
-  if(read(socket_client, buffer, sizeof(buffer)) !=-1){
+    if(read(socket_client, buffer, sizeof(buffer)) !=-1){
     a = getData(buffer);
-  }*/
+    }*/
   //--print statements--//
-	printf("%s\n", game_header);
-	printf("\n");
-	printf("%i\n", a);
-	printf("\n");
-	strcpy(current_status, " ");
-	strcat(current_status, KGRN);
-	strcpy(current_status, completed_words);
-	strcat(current_status, KGRN);
-	strcat(current_status, current_word); 
-	strcat(current_status, " "); 
-	strcat(current_status, KWHT);
-	strcat(current_status, rwp);
-	printf("%s\n", current_status);
-	printf("\n");
-	printf("%s\n", current_typed);
-	buffer[0] = 0;
+  printf("%s\n", game_header);
+  printf("\n");
+  printf("%i\n", a);
+  printf("\n");
+  strcpy(current_status, " ");
+  strcat(current_status, KGRN);
+  strcpy(current_status, completed_words);
+  strcat(current_status, KGRN);
+  strcat(current_status, current_word); 
+  strcat(current_status, " "); 
+  strcat(current_status, KWHT);
+  strcat(current_status, rwp);
+  printf("%s\n", current_status);
+  printf("\n");
+  printf("%s\n", current_typed);
+  buffer[0] = 0;
 }
 char * makeData(){
-    char* str;
-    sprintf(str, "%d", number_of_words);
-    return str;
+  char* str;
+  sprintf(str, "%d", number_of_words);
+  return str;
 }
 int getData(char* str){
-    char *eptr;
-    long result;
-    result = strtol(str, &eptr, 10);
-    return (int) (long) result;
+  char *eptr;
+  long result;
+  result = strtol(str, &eptr, 10);
+  return (int) (long) result;
 }
--- 5/krzysztofhochlewicz_kaizencastanos_bilingwu/player2.c
+++ 5/krzysztofhochlewicz_kaizencastanos_bilingwu/player2.c
@@ -49,7 +49,7 @@ int getData(char* str);
 int main(){
   
   //--------------------//
-  //----Server Stuff----//
+  //----Network Stuff----//
   //--------------------//
   
   //creating the socket 
@@ -81,52 +81,71 @@ int main(){
   //----The Actual Game Bit----//
   //---------------------------//
   clock_t start = clock(), diff;
-	printf("\033[2J\033[1;1H"); //clear the screen
-	printf("%s\n", game_header);
-	printf("\n");
-	printf("%s\n", remaining_words);
+  printf("\033[2J\033[1;1H"); //clear the screen
+  printf("%s\n", game_header);
+  printf("\n");
+  printf("%s\n", remaining_words);
 	
-	current_word = strsep(&rwp, delim); //get the first word
+  current_word = strsep(&rwp, delim); //get the first word
 
-	int check_1 = 0; //Used to fix a bug where a space kept appearing after you typed a word
-	int check_2 = 1; //To keep the first check from affecting the first word
-	while(current_word != NULL){ 				 //loop over words until there are no more words
-		entered_chars = 0;
-		current_typed[0] = 0;
-		if(check_2 != 1)
-			check_1 = 1;
-		while(strcmp(current_word, current_typed)){ //loop until the user enters the current word correctly
-			current_char = get_letter(); //wait for a key to be pressed
-			if (current_char ==  127){   //if backspace pressed
-				if (entered_chars > 0){  //if there are entered chars for the current word
-					entered_chars--;
-					current_typed[strlen(current_typed)-1] = 0; //get rid of last letter
-				}
-			}
-			else{ //if not a backspace
-				entered_chars++;
-				current_typed[strlen(current_typed)+1] = 0;
-				current_typed[strlen(current_typed)] = current_char;
-			}
-			printf("\033[2J\033[1;1H"); //clear the screen
-			update_screen(); //update status
-			
-			if(check_1)
-				current_typed[0] = 0;
-			check_1 = 0;
-		}
-		strcat(completed_words, current_word);
-		strcat(completed_words, " ");
-		current_word = strsep(&rwp, delim);
-		check_2 = 0;
-		number_of_words++;
+  int check_1 = 0; //Used to fix a bug where a space kept appearing after you typed a word
+  int check_2 = 1; //To keep the first check from affecting the first word
+  while(current_word != NULL){ 				 //loop over words until there are no more words
+    entered_chars = 0;
+    current_typed[0] = 0;
+    if(check_2 != 1)
+      check_1 = 1;
+    while(strcmp(current_word, current_typed)){ //loop until the user enters the current word correctly
+      current_char = get_letter(); //wait for a key to be pressed
+      if (current_char ==  127){   //if backspace pressed
+	if (entered_chars > 0){  //if there are entered chars for the current word
+	  entered_chars--;
+	  current_typed[strlen(current_typed)-1] = 0; //get rid of last letter
 	}
+      }
+      else{ //if not a backspace
+	entered_chars++;
+	current_typed[strlen(current_typed)+1] = 0;
+	current_typed[strlen(current_typed)] = current_char;
+      }
+      printf("\033[2J\033[1;1H"); //clear the screen
+      update_screen(); //update status
+			
+      if(check_1)
+	current_typed[0] = 0;
+      check_1 = 0;
+    }
+    strcat(completed_words, current_word);
+    strcat(completed_words, " ");
+    current_word = strsep(&rwp, delim);
+    check_2 = 0;
+    number_of_words++;
 
-	//Return the time
-	diff = clock() - start;
-	int msec = diff * 1000 / CLOCKS_PER_SEC;
-	printf("\nTime taken: %d seconds %d milliseconds\n", msec/1000, msec%1000); //Exact time
-	printf("Words per minute: %f WPM\n", (13.0/(msec/1000))* 60); //words divided by seconds, multiplied by 60 seconds
+    //THUS BEGINS THE NETWORKING STUFF, MAY THE ODDS BE EVER IN MY FAVOR
+    /*
+      send(socket_id, number_of_words, sizeof(number_of_words), 0);
+      recv(socket_id, buffer, sizeof(buffer), 0);
+      printf("<client> received [%s]\n\n", buffer);
+    */
+    int received_int = 0;
+    int return_status;
+    return_status = read(socket_id, &received_int, sizeof(received_int));
+    if (return_status > 0) {
+      fprintf(stdout, "Received int = %d\n", ntohl(received_int));
+    }
+    else {
+      printf("<client>Failed to read");
+    }
+    int converted_number = htonl(number_of_words);
+    write(socket_id, &converted_number, sizeof(converted_number));
+    //If there is ever a need to comment new code out, end it here
+  }
+
+  //Return the time
+  diff = clock() - start;
+  int msec = diff * 1000 / CLOCKS_PER_SEC;
+  printf("\nTime taken: %d seconds %d milliseconds\n", msec/1000, msec%1000); //Exact time
+  printf("Words per minute: %f WPM\n", (13.0/(msec/1000))* 60); //words divided by seconds, multiplied by 60 seconds
 
   
   return 0;
@@ -135,35 +154,35 @@ int main(){
 int a = 0;
 void update_screen(){
   /*if(read(socket_id, buffer, sizeof(buffer)) !=-1){
-  	a = getData(buffer);
-  }
-  write(socket_id, makeData(), 6);*/
+    a = getData(buffer);
+    }
+    write(socket_id, makeData(), 6);*/
   //--print statements--//
-	printf("%s\n", game_header);
-	printf("\n");
-	printf("%i\n", a);
-	printf("\n");
-	strcpy(current_status, " ");
-	strcat(current_status, KGRN);
-	strcpy(current_status, completed_words);
-	strcat(current_status, KGRN);
-	strcat(current_status, current_word); 
-	strcat(current_status, " "); 
-	strcat(current_status, KWHT);
-	strcat(current_status, rwp);
-	printf("%s\n", current_status);
-	printf("\n");
-	printf("%s\n", current_typed);
-	buffer[0] = 0;
+  printf("%s\n", game_header);
+  printf("\n");
+  printf("%i\n", a);
+  printf("\n");
+  strcpy(current_status, " ");
+  strcat(current_status, KGRN);
+  strcpy(current_status, completed_words);
+  strcat(current_status, KGRN);
+  strcat(current_status, current_word); 
+  strcat(current_status, " "); 
+  strcat(current_status, KWHT);
+  strcat(current_status, rwp);
+  printf("%s\n", current_status);
+  printf("\n");
+  printf("%s\n", current_typed);
+  buffer[0] = 0;
 }
 char * makeData(){
-    char* str;
-    sprintf(str, "%d", number_of_words);
-    return str;
+  char* str;
+  sprintf(str, "%d", number_of_words);
+  return str;
 }
 int getData(char* str){
-    char *eptr;
-    long result;
-    result = strtol(str, &eptr, 10);
-    return (int) (long) result;
+  char *eptr;
+  long result;
+  result = strtol(str, &eptr, 10);
+  return (int) (long) result;
 }

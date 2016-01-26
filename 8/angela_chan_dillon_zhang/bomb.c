31a4abe3de98dea508e99d312f4f044703263b2c
Yicheng Wang <alex.wyc2098@protonmail.ch>
2016-01-26 03:19:35 -0500

    reverted 7c85b08

--- 8/angela_chan_dillon_zhang/bomb.c
+++ 8/angela_chan_dillon_zhang/bomb.c
@@ -7,13 +7,11 @@
 #include <sys/stat.h>
 
 #include "color.c"
-#include "bomb_helper.h"
 
 int main() {
   textcolor(BRIGHT, RED, BLACK);
   printf("This is the Bomb component.\n");
   textcolor(RESET, WHITE, BLACK);
-
-  create_module("./asciiart/symbols/s1.dat");
+  
   return 0;
 }
--- 8/jake_waksbaum/hostname.h
+++
@@ -1,8 +0,0 @@
-#ifndef HOSTNAME_H
-#define HOSTNAME_H
-
-#include <netinet/in.h>
-
-int hostname_to_ip(char * hostname, struct in_addr** addr);
-
-#endif
